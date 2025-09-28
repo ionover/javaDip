090edcb3b1e5f7277837cb149685608b9ae14450
v.ionov <v.ionov@mycrg.ru>
2025-09-28 13:38:52 +0300

    init

---
+++ src/main/java/ru/mycrg/javadip/JavaDipApplication.java
@@ -0,0 +1,12 @@
+package ru.mycrg.javadip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDipApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/mycrg/javadip/JavaDipApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.mycrg.javadip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
