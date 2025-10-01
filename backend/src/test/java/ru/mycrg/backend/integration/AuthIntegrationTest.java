bc530558f538ccd7bc4444ad7e8aa6ec6808f17a
v.ionov <v.ionov@mycrg.ru>
2025-10-01 18:00:29 +0300

    tests

--- backend/src/test/java/ru/mycrg/backend/integration/AuthIntegrationTest.java
+++ backend/src/test/java/ru/mycrg/backend/integration/AuthIntegrationTest.java
@@ -1,38 +1,38 @@
-//package ru.mycrg.backend.integration;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.http.HttpEntity;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.client.RestTemplate;
-//import org.testcontainers.junit.jupiter.Testcontainers;
-//import org.testcontainers.shaded.com.github.dockerjava.core.MediaType;
-//import ru.mycrg.backend.dto.LoginDto;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//@Testcontainers
-//public class AuthIntegrationTest {
-//
-//    @Test
-//    void successfullyAuthWithCorrectCredentials() {
-//        RestTemplate restTemplate = new RestTemplate();
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        LoginDto requestBody = new LoginDto("admin", "admin");
-//
-//        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
-//
-//        // Выполняем запрос
-//        ResponseEntity<String> response = restTemplate.postForEntity(
-//                "http://10.10.10.61:8084/login",
-//                entity,
-//                String.class
-//        );
-//
-//        // Проверяем результат
-//        assertTrue(response.getStatusCode().is2xxSuccessful());
-//        assertTrue(response.getBody() != null && response.getBody().contains("auth-token"));
-//    }
-//}
+package ru.mycrg.backend.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.github.dockerjava.core.MediaType;
+import ru.mycrg.backend.dto.LoginDto;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
+public class AuthIntegrationTest {
+
+    @Test
+    void successfullyAuthWithCorrectCredentials() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        LoginDto requestBody = new LoginDto("admin", "admin");
+
+        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+
+        // Выполняем запрос
+        ResponseEntity<String> response = restTemplate.postForEntity(
+                "http://10.10.10.61:8084/login",
+                entity,
+                String.class
+        );
+
+        // Проверяем результат
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertTrue(response.getBody() != null && response.getBody().contains("auth-token"));
+    }
+}
--- backend/src/test/java/ru/mycrg/backend/integration/IntegrationTestContainersConfig.java
+++ backend/src/test/java/ru/mycrg/backend/integration/IntegrationTestContainersConfig.java
@@ -1,45 +1,44 @@
-//package ru.mycrg.backend.integration;
-//
-//import org.testcontainers.containers.GenericContainer;
-//import org.testcontainers.containers.Network;
-//import org.testcontainers.containers.PostgreSQLContainer;
-//import org.testcontainers.containers.wait.strategy.Wait;
-//import org.testcontainers.junit.jupiter.Container;
-//import org.testcontainers.junit.jupiter.Testcontainers;
-//import org.testcontainers.utility.DockerImageName;
-//
-//@Testcontainers
-//public class IntegrationTestContainersConfig {
-//
-//    // Create a shared network for all containers
-//    static Network network = Network.newNetwork();
-//
-//    @Container
-//    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17")
-//            .withDatabaseName("backend_db")
-//            .withUsername("pgAdmin")
-//            .withPassword("pdPass")
-//            .withNetwork(network)
-//            .withNetworkAliases("postgres")
-//            .withInitScript("init-db.sql");
-//
-//    @Container
-//    static GenericContainer<?> backend = new GenericContainer<>(DockerImageName.parse("javadip-backend:latest"))
-//            .withExposedPorts(8084)
-//            .withNetwork(network)
-//            .withNetworkAliases("backend")
-//            .withEnv("SPRING_FLYWAY_USER", "pgAdmin")
-//            .withEnv("SPRING_FLYWAY_PASSWORD", "pdPass")
-//            .withEnv("SPRING_DATASOURCE_URL", "jdbc:postgresql://postgres:5432/backend_db")
-//            .withEnv("SPRING_DATASOURCE_USERNAME", "pgAdmin")
-//            .withEnv("SPRING_DATASOURCE_PASSWORD", "pdPass")
-//            .dependsOn(postgres)
-//            .waitingFor(Wait.forHttp("/actuator/health").forPort(8084));
-//
-//    @Container
-//    static GenericContainer<?> frontend = new GenericContainer<>(DockerImageName.parse("javadip-frontend:latest"))
-//            .withExposedPorts(80)
-//            .withNetwork(network)
-//            .withNetworkAliases("frontend")
-//            .waitingFor(Wait.forHttp("/").forPort(80));
-//}
+package ru.mycrg.backend.integration;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+public class IntegrationTestContainersConfig {
+
+    static Network network = Network.newNetwork();
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17")
+            .withDatabaseName("backend_db")
+            .withUsername("pgAdmin")
+            .withPassword("pdPass")
+            .withNetwork(network)
+            .withNetworkAliases("postgres")
+            .withInitScript("init-db.sql");
+
+    @Container
+    static GenericContainer<?> backend = new GenericContainer<>(DockerImageName.parse("javadip-backend:latest"))
+            .withExposedPorts(8084)
+            .withNetwork(network)
+            .withNetworkAliases("backend")
+            .withEnv("SPRING_FLYWAY_USER", "pgAdmin")
+            .withEnv("SPRING_FLYWAY_PASSWORD", "pdPass")
+            .withEnv("SPRING_DATASOURCE_URL", "jdbc:postgresql://postgres:5432/backend_db")
+            .withEnv("SPRING_DATASOURCE_USERNAME", "pgAdmin")
+            .withEnv("SPRING_DATASOURCE_PASSWORD", "pdPass")
+            .dependsOn(postgres)
+            .waitingFor(Wait.forHttp("/actuator/health").forPort(8084));
+
+    @Container
+    static GenericContainer<?> frontend = new GenericContainer<>(DockerImageName.parse("javadip-frontend:latest"))
+            .withExposedPorts(80)
+            .withNetwork(network)
+            .withNetworkAliases("frontend")
+            .waitingFor(Wait.forHttp("/").forPort(80));
+}
