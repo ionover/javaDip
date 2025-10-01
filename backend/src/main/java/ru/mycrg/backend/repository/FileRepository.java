ef14dff1f9bc6b28e00ac7da59e402b241ee528b
v.ionov <v.ionov@mycrg.ru>
2025-10-01 16:01:18 +0300

    load file

--- backend/src/main/java/ru/mycrg/backend/repository/FileRepository.java
+++ backend/src/main/java/ru/mycrg/backend/repository/FileRepository.java
@@ -3,6 +3,7 @@ package ru.mycrg.backend.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.mycrg.backend.entity.FilesEntity;
 
@@ -16,6 +17,6 @@ public interface FileRepository extends JpaRepository<FilesEntity, UUID> {
     @Query("SELECT f FROM FilesEntity f WHERE f.isDeleted = false ORDER BY f.id DESC")
     List<FilesEntity> findAllNotDeleted(Pageable pageable);
 
-    @Query("SELECT f FROM FilesEntity f WHERE f.title = :filename AND f.isDeleted = false")
-    Optional<FilesEntity> findByTitleAndNotDeleted(String filename);
+    @Query("SELECT f FROM FilesEntity f WHERE f.title = :title AND f.isDeleted = false")
+    Optional<FilesEntity> findByTitleAndNotDeleted(@Param("title") String title);
 }
