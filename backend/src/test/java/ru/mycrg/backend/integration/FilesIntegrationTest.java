f284c59b8381f572b6b505715103b1cacb43509b
v.ionov <v.ionov@mycrg.ru>
2025-10-02 16:26:29 +0300

    tests

--- backend/src/test/java/ru/mycrg/backend/integration/FilesIntegrationTest.java
+++ backend/src/test/java/ru/mycrg/backend/integration/FilesIntegrationTest.java
@@ -29,12 +29,16 @@ public class FilesIntegrationTest {
 
     @Test
     void emptyListWhenNoFilesAdd() {
-        //Scenario: Если в облаке нет никаких файлов, то получим пустой массив
+        //Scenario: Если в облаке нет файлов, то с валидным токеном получим пустой массив
         //Given я авторизован как "admin"
+        //*     на сервере нет файлов
         //When  я делаю запрос на получения списка файлов
         //Then  сервер отвечает со статусом: 200
         //And   в ответе находится пустой массив
         String authToken = extractTokenFromResponse(loginAsAdmin());
+
+        deleteAllIfExist(authToken);
+
         ResponseEntity<List<FilesDto>> response = getFilesList(authToken);
 
         assertTrue(response.getStatusCode().is2xxSuccessful());
@@ -44,14 +48,16 @@ public class FilesIntegrationTest {
 
     @Test
     public void successfullyAddFile() {
-        //Scenario: Добавление файла, с правильным токеном проходит успешно
+        //Scenario: Добавление файла с валидным токеном проходит успешно
         //Given я авторизован как "admin"
+        //*     на сервере нет файлов
         //When  я делаю запрос на добавления файла
         //Then  сервер отвечает со статусом: 200
         //And   имя файла корректно
         String authToken = extractTokenFromResponse(loginAsAdmin());
-        String filename = "smallTestFileOne.txt";
+        deleteAllIfExist(authToken);
 
+        String filename = "smallTestFileOne.txt";
         ResponseEntity<FilesDto> response = addFileOnCloud(authToken, filename);
 
         assertTrue(response.getStatusCode().is2xxSuccessful());
@@ -60,14 +66,17 @@ public class FilesIntegrationTest {
 
     @Test
     public void successfullyPutFile() {
-        //Scenario: Обновление файла, с правильным токеном проходит успешно
+        //Scenario: Обновление файла с валидным токеном проходит успешно
         //Given я авторизован как "admin"
-        //Given на сервер загружен файл с именем "smallTestFileTwo.txt"
+        //*     на сервере нет файлов
+        //*     на сервер загружен файл с именем "smallTestFileTwo.txt"
         //When  я меняю имя файла на "helloWorld.txt"
         //Then  сервер отвечает со статусом: 200
         //And   в ответе имя файла соответствует ожидаемому
         //*     вес файла НЕ изменился
         String authToken = extractTokenFromResponse(loginAsAdmin());
+        deleteAllIfExist(authToken);
+
         String filename = "smallTestFileTwo.txt";
         ResponseEntity<FilesDto> res = addFileOnCloud(authToken, filename);
         FilesDto currentFile = res.getBody();
@@ -86,6 +95,50 @@ public class FilesIntegrationTest {
         }
     }
 
+    @Test
+    public void successfullyDeleteFile() {
+        //Scenario: Удаление файла с валидным токеном проходит успешно
+        //Given я авторизован как "admin"
+        //*     на сервере нет файлов
+        //*     на сервер загружен файл с именем "smallTestFileThree.txt"
+        //When  я удаляю файл с именем "smallTestFileThree.txt"
+        //Then  сервер отвечает со статусом: 200
+        //And   файла с таким именем больше нет на сервере
+        String authToken = extractTokenFromResponse(loginAsAdmin());
+        deleteAllIfExist(authToken);
+        String filename = "smallTestFileThree.txt";
+        addFileOnCloud(authToken, filename);
+
+        deleteFile(authToken, filename);
+        ResponseEntity<List<FilesDto>> response = getFilesList(authToken);
+
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertNotNull(response.getBody());
+        assertTrue(response.getBody().isEmpty());
+    }
+
+    @Test
+    public void successfullyAddThreeFiles() {
+        //Scenario: C валидным токеном можно добавить несколько файлов
+        //Given я авторизован как "admin"
+        //*     на сервере нет файлов
+        //When  я последовательно добавляю на сервер три файла
+        //And   все 3 файла есть на сервере
+        String authToken = extractTokenFromResponse(loginAsAdmin());
+        deleteAllIfExist(authToken);
+
+        String[] filenames = new String[]{"smallTestFileOne.txt", "smallTestFileTwo.txt", "smallTestFileThree.txt"};
+
+        for (String filename: filenames) {
+            addFileOnCloud(authToken, filename);
+        }
+
+        ResponseEntity<List<FilesDto>> response = getFilesList(authToken);
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertNotNull(response.getBody());
+        assertEquals(filenames.length, response.getBody().size());
+    }
+
     private ResponseEntity<List<FilesDto>> getFilesList(String authToken) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -98,7 +151,7 @@ public class FilesIntegrationTest {
                 "http://10.10.10.61:8084/list?limit=3",
                 GET,
                 entity,
-                new ParameterizedTypeReference<List<FilesDto>>() {
+                new ParameterizedTypeReference<>() {
                 }
         );
     }
@@ -152,5 +205,38 @@ public class FilesIntegrationTest {
                 entity,
                 Void.class
         );
+    }
+
+    private void deleteFile(String authToken, String filename) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(AUTH_TOKEN_HEADER, "Bearer " + authToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        restTemplate.exchange(
+                "http://10.10.10.61:8084/file?filename=" + filename,
+                DELETE,
+                entity,
+                Void.class
+        );
+    }
+
+    private void deleteAllIfExist(String authToken) {
+        ResponseEntity<List<FilesDto>> response = getFilesList(authToken);
+
+        if (response.getBody() == null || response.getBody().isEmpty()) {
+            System.out.println("Нет файлов чтобы удалять");
+            return;
+        }
+
+        List<FilesDto> files = response.getBody();
+        for (FilesDto file: files) {
+            String filename = file.getFilename();
+            System.out.println("Удаляем файл: " + filename);
+
+            deleteFile(authToken, filename);
+        }
     }
 }
